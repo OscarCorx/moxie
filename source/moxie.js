4f9a97b3876dfcbf0e2626085ba11de24320b094
OscarCorx <151416586+OscarCorx@users.noreply.github.com>
2026-06-02 14:15:24 -0500

    Development

--- source/moxie.js
+++ source/moxie.js
@@ -111,4 +111,21 @@ class Process {
 const PROCESS = new Process();
 PROCESS.start();
 
+function load(source) {
+  const script = document.createElement("script");
+  script.setAttribute("src", source);
+  script.onload = () => {
+    console.log(`[${source}] LOADED.`);
+  };
+  script.onerror = () => {
+    console.log(`[${source}] FAILED.`);
+  };
+  document.head.appendChild(script);
+}
+
+window.addEventListener("load", () => {
+  console.log("WINDOW LOAD");
+  // load("./moxie/moxie.js");
+});
+
 // PROCESS.emit("kind", { foo: "bar" }, [1, 2, 3]);
